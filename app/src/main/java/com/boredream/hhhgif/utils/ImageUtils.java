2a6c31e23b16b73e246006c264520eb6a2dddd7f
boredream <48262906@qq.com>
2016-01-29 18:24:27 +0800

    createVideoThumbnail

--- app/src/main/java/com/boredream/hhhgif/utils/ImageUtils.java
+++ app/src/main/java/com/boredream/hhhgif/utils/ImageUtils.java
@@ -8,11 +8,16 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.media.MediaMetadataRetriever;
+import android.media.ThumbnailUtils;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
+
+import java.util.HashMap;
 
 
 public class ImageUtils {
@@ -272,5 +277,44 @@ public class ImageUtils {
 //
 //        return bytes;
 //    }
+
+    /**
+     * 获取网络图片缩略图
+     *
+     * @param url
+     * @param width
+     * @param height
+     * @return
+     */
+    public static Bitmap createVideoThumbnail(String url, int width, int height) {
+
+        Bitmap bitmap = null;
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        int kind = MediaStore.Video.Thumbnails.MINI_KIND;
+        try {
+            if (Build.VERSION.SDK_INT >= 14) {
+                retriever.setDataSource(url, new HashMap<String, String>());
+            } else {
+                retriever.setDataSource(url);
+            }
+            bitmap = retriever.getFrameAtTime();
+        } catch (IllegalArgumentException ex) {
+            // Assume this is a corrupt video file
+        } catch (RuntimeException ex) {
+            // Assume this is a corrupt video file.
+        } finally {
+            try {
+                retriever.release();
+            } catch (RuntimeException ex) {
+                // Ignore failures while cleaning up.
+            }
+        }
+
+        if (kind == MediaStore.Images.Thumbnails.MICRO_KIND && bitmap != null) {
+            bitmap = ThumbnailUtils.extractThumbnail(bitmap, width, height, ThumbnailUtils.OPTIONS_RECYCLE_INPUT);
+        }
+
+        return bitmap;
+    }
 
 }
